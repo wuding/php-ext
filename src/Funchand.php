89dde7a4ec65fd0e5d16dbade28f33352b4b4e8e
wuding <netjoin@gmail.com>
2019-03-31 11:48:39 +0800

    新增函数处理扩展类

---
+++ src/Funchand.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * 函数处理
+ */
+
+namespace Ext;
+
+class Funchand
+{
+	/**
+	 * 用参数数组调用一个回调
+	 */
+	public static function call_user_func_array($function, $param_arr)
+	{
+
+	}
+
+	/**
+	 * 用第一个参数作为回调函数调用
+	 */
+	public static function call_user_func($function)
+	{
+
+	}
+
+	/**
+	 * 创建一个匿名函数 lambda-style Λ
+	 */
+	public static function create_function($args, $code)
+	{
+
+	}
+
+	/**
+	 * 调用一个静态方法并传递句柄参数数组
+	 */
+	public static function forward_static_call_array($function, $parameters)
+	{
+
+	}
+
+	/**
+	 * 调用一个静态方法
+	 */
+	public static function forward_static_call($function)
+	{
+
+	}
+
+	/**
+	 * 返回一个项目从论点列表
+	 */
+	public static function func_get_arg($arg_num)
+	{
+
+	}
+
+	/**
+	 * 返回一个数组构成函数论点列表
+	 */
+	public static function func_get_args($oid)
+	{
+
+	}
+
+	/**
+	 * 返回 TRUE 如果给的函数被定义的
+	 */
+	public static function function_exists($function_name)
+	{
+
+	}
+
+	/**
+	 * 返回定义的函数数组
+	 */
+	public static function get_defiend_functions()
+	{
+
+	}
+
+	/**
+	 * 注册一个会在 php 中止执行函数
+	 */
+	public static function register_shutdown_function($function)
+	{
+
+	}
+
+	/**
+	 * 注册一个打上勾的执行函数
+	 */
+	public static function register_tick_function($function)
+	{
+
+	}
+
+	/**
+	 * 注销·一个打上勾的执行函数
+	 */
+	public static function unregister_tick_function($function_name)
+	{
+
+	}
+}
