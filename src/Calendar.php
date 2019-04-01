a6b96b21366609b28bfda5a1ac0c344ab7a9d5f7
wuding <netjoin@gmail.com>
2019-04-01 10:05:48 +0800

    新增日历函数类

---
+++ src/Calendar.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * 日历函数类
+ */
+
+namespace Ext;
+
+class Calendar
+{
+    
+    public function __construct($argument)
+    {
+        # code...
+    }
+
+    /**
+     * 返回一个月的天数从给的一年及日历
+     */
+    public static function cal_days_in_month($calendar, $month, $year)
+    {
+
+    }
+
+    /**
+     * 转换从 Julian 日数到支持的日历
+     */
+    public static function cal_from_jd($jd, $calendar)
+    {
+
+    }
+
+    /**
+     * 返回信息关于一个特定日历
+     */
+    public static function cal_info()
+    {
+
+    }
+
+    /**
+     * 转换从一个支持的日历到 Julian 日数
+     */
+    public static function cal_to_jd($calendar, $month, $day, $year)
+    {
+
+    }
+
+    /**
+     * 获取 Unix 时间戳午夜复活节从给定的一年
+     */
+    public static function easter_date()
+    {
+
+    }
+
+    /**
+     * 获取日数在 3 月 21 哪一个复活节之间天数
+     */
+    public static function easter_days()
+    {
+
+    }
+
+    /**
+     * 转换一个日期从法国公共日历到 Julian 日数
+     */
+    public static function frenchtojd($month, $day, $year)
+    {
+
+    }
+
+    /**
+     * 转换一个 Gregorian 日期到 Julian 日数
+     */
+    public static function GregorianToJD($month, $day, $year)
+    {
+
+    }
+
+    /**
+     * 返回一周天数
+     */
+    public static function JDDayOfWeek($julianday)
+    {
+
+    }
+
+    /**
+     * 返回一个月份名称
+     */
+    public static function JDMonthName($julianday, $mode)
+    {
+
+    }
+
+    /**
+     * 转换一个 Julian 天数到法国共和日历
+     */
+    public static function JDToFrench($juliandaycount)
+    {
+
+    }
+
+    /**
+     * 转换 Julian 天数到 Gregorian 日期
+     */
+    public static function JDToGregorian($julianday)
+    {
+
+    }
+
+    /**
+     * 转换朱利安天数到 Jewish 日历日期
+     */
+    public static function jdtojewish($juliandaycount) 
+    {
+
+    }
+
+    /**
+     * 转换朱利安天数到朱利安日历日期
+     */
+    public static function JDToJulian($julianday)
+    {
+
+    }
+
+    /**
+     * 转换朱利安日期到 Unix 时间戳
+     */
+    public static function jdtounix($jday)
+    {
+
+    }
+
+    /**
+     * 转换一个日期从犹太人的日历到朱利安天数
+     */
+    public static function JewishToJD($month, $day, $year)
+    {
+
+    }
+
+    /**
+     * 转换 Julian 日历日期到 Julian 天数
+     */
+    public static function JulianToJD($month, $day, $year)
+    {
+
+    }
+
+    /**
+     * 转换 Unix 时间戳到 Julian 日子
+     */
+    public static function unixtojd()
+    {
+
+    }
+
+}
