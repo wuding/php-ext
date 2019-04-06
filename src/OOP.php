d9bd46921cdf4986f6f03401ef5dcd4f5f84ff02
wuding <netjoin@gmail.com>
2019-04-06 22:23:27 +0800

    新增面向对象程序设计函数类

---
+++ src/OOP.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * 面向对象程序设计
+ */
+
+namespace Ext;
+
+class OOP
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Property Overloading
+     */
+    public function __set($name, $value)
+    {
+
+    }
+
+    public function __get($name)
+    {
+
+    }
+
+    public function __isset($name)
+    {
+
+    }
+
+    public function __unset($name)
+    {
+        
+    }
+
+    /**
+     * Method Overloading
+     */
+    public function __call($name, $arguments)
+    {
+
+    }
+
+    public function __callStatic($name, $arguments)
+    {
+        
+    }
+}
