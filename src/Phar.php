3157e200ea98406b0816508b36e7819bb7915a6e
wuding <netjoin@gmail.com>
2019-04-09 11:08:09 +0800

    新增 Phar 扩展类

---
+++ src/Phar.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * PHP 存档
+ */
+
+namespace Ext;
+
+class Phar
+{
+    /**
+     * 构建函数 
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 添加一个空目录到 phar 档案
+     */
+    public function addEmptyDir()
+    {
+
+    }
+
+    /**
+     * 从文件系统添加一个文件到 phar 档案
+     */
+    public function addFile()
+    {
+
+    }
+
+    /**
+     * 从一个字符串添加一个文件到 phar 存档文件
+     */
+    public function addFromString()
+    {
+
+    }
+
+    /**
+     * 返回 API  版本 
+     */
+    public function apiVersion()
+    {
+
+    }
+
+    /**
+     * 构建一个 phar 档案文件从一个目录内部的文件
+     */
+    public function buildFromDirectory()
+    {
+
+    }
+
+    /**
+     * 构建一个 phar 档案文件从一个迭代器
+     */
+    public function buildeFromIterator()
+    {
+
+    }
+
+    /**
+     * 返回是否 phar 扩展名支持压缩使用 zlib 或 bzip2 之一
+     */
+    public function canCompress()
+    {
+
+    }
+
+    /**
+     * 返回 phar 扩展名是否支持写和创建 phars
+     */
+    public function canWrite()
+    {
+
+    }
+
+    /**
+     * 
+}
