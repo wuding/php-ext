c2cb32652509eb7423d2351dbddc2924b4c67af0
wuding <netjoin@gmail.com>
2019-04-15 19:29:34 +0800

    修复 null 合并，获取键值对，设置供应商目录和超级变量

--- src/JSON/ComposerJSON.php
+++ src/JSON/ComposerJSON.php
@@ -10,6 +10,8 @@ use Ext\JSON;
 
 class ComposerJSON extends JSON
 {
+    public static $vendorDir = '';
+
     /**
      * 构建函数
      */
@@ -23,19 +25,20 @@ class ComposerJSON extends JSON
      */
     public static function getAutoload($dev = null)
     {
-        if ($dev) {
-            return self::$json_decoded->autoload;
+        if (!$dev) {
+            return parent::$json_decoded->autoload;
         }
-        return self::$josn_decoded->{'autoload-dev'};
+        return $notation = parent::$json_decoded->{'autoload-dev'} ?? null;
     }
 
     /**
      * PSR 4
      */
-    public static function getPsr4($dev = null)
+    public static function getPsr4($dev = null, $format = false)
     {
         $autoload = self::getAutoload($dev);
-        return (array) $autoload->{'psr-4'};
+        $notation = $autoload->{'psr-4'} ?? null;
+        return (array) $array = $format ? self::getKeyValue($notation) : $notation;
     }
 
     /**
@@ -50,10 +53,11 @@ class ComposerJSON extends JSON
     /**
      * files
      */
-    public static function getFiles($dev = null)
+    public static function getFiles($dev = null, $format = true)
     {
         $autoload = self::getAutoload($dev);
-        return (array) $autoload->{'files'};
+        $notation = $autoload->{'files'} ?? null;
+        return (array) $array = $format ? self::geTKeyValue($notation) : $notation;
     }
 
     /**
@@ -63,5 +67,42 @@ class ComposerJSON extends JSON
     {
         $autoload = self::getAutoload($dev);
         return (array) $autoload->{'classmap'};
+    }
+
+    /**
+     * reset value path
+     */
+    public static function getKeyValue($array = [])
+    {
+        foreach ($array as $key => &$value) {
+            $value = trim($value, '/');
+            if (!preg_match('/:/', $value)) {
+                $value = self::$vendorDir . $value;
+            }
+            $value = str_replace('\\', '/', $value);
+        }
+        return $array;
+    }
+
+    /**
+     * setting vendorDir
+     */
+    public static function setVendorDir($vendorDir = null)
+    {
+        if ($vendorDir) {
+            $vendorDir .= '/';
+        }
+        $vendorDir = str_replace('\\', '/', $vendorDir);
+        self::$vendorDir = $vendorDir;
+    }
+
+    /**
+     * set super vars
+     */
+    public static function setSuperVars()
+    {
+        global $ANFORA_FILE, $ANFORA_RULE;
+        $ANFORA_FILE = self::getFiles();
+        $ANFORA_RULE = self::getPsr4(0, true);
     }
 }
