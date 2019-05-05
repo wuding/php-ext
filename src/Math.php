4ced9e1cf7f576db6e0660c4a977c0e4a8d8921e
netjoin <netjoin@gmail.com>
2019-05-05 18:17:27 +0800

    新增數學函數類 Math

---
+++ src/Math.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Mathematical Functions Class
+ */
+
+namespace Ext;
+
+class Math
+{
+    /**
+     * Predefined Constants
+     *
+     * @var        array
+     */
+    public static $constants = [];
+
+    /**
+     *  構建函數
+     */
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * 初始化方法
+     */
+    public function init()
+    {
+        self::$constants = [
+            'M_PI' => M_PI,
+            'M_E' => M_E,
+            'M_LOG2E' => M_LOG2E,
+            'M_LOG10E' => M_LOG10E,
+            'M_LN2' => M_LN2,
+            'M_LN10' => M_LN10,
+            'M_PI_2' => M_PI_2,
+            'M_PI_4' => M_PI_4,
+            'M_1_PI' => M_1_PI,
+            'M_2_PI' => M_2_PI,
+            'M_SQRTPI' => M_SQRTPI,
+            'M_2_SQRTPI' => M_2_SQRTPI,
+            'M_SQRT2' => M_SQRT2,
+            'M_SQRT3' => M_SQRT3,
+            'M_SQRT1_2' => M_SQRT1_2,
+            'M_LNPI' => M_LNPI,
+            'M_EULER' => M_EULER,
+            'PHP_ROUND_HALF_UP' => PHP_ROUND_HALF_UP,
+            'PHP_ROUND_HALF_DOWN'=> PHP_ROUND_HALF_DOWN,
+            'PHP_ROUND_HALF_EVEN' => PHP_ROUND_HALF_EVEN,
+            'PHP_ROUND_HALF_ODD' => PHP_ROUND_HALF_ODD,
+            'NAN' => NAN,
+            'INF' => INF,
+        ];
+    }
+
+    /**
+     * Round fractions down
+     *
+     * @param      float    $value     The value
+     * @param      integer  $decimals  The decimals
+     *
+     * @return     float    ( description_of_the_return_value )
+     */
+    public static function floor(float $value, int $decimals = 0) : float
+    {
+        if (0 < $decimals) {
+            return self::numberFloor($value, $decimals);
+        }
+        return floor($value);
+    }
+
+    public static function numberFloor(float $value, int $decimals = 0)
+    {
+        if (!preg_match('/\./', $value)) {
+            return $value;
+        }
+        
+        $exp = explode('.', $value);
+        $len = strlen($exp[1]);
+        if (2 < $len) {
+            $exp[1] = substr($exp[1], 0, $decimals);
+        }
+        return $imp = implode('.', $exp);
+    }
+}
