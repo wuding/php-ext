be1ac95cedf56be250fb6c08c2eeaf499b42d039
wuding <netjoin@gmail.com>
2019-05-15 23:19:59 +0800

    新增输出缓冲控制函数类

---
+++ src/OutControl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 输出缓冲控制
+ */
+namespace Ext;
+
+class OutControl
+{
+	/**
+	 * 打开/关闭绝对刷送
+	 */
+	public static function implicitFlush(int $flag = 1)
+	{
+		return ob_implicit_flush($flag);
+	}
+}
