1f8f80c65be20d31a34ebe763a283d4d26055625
wuding <netjoin@gmail.com>
2019-05-22 16:17:36 +0800

    created Hash class filename

---
+++ src/Hash.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Ext;
+
+class Hash
+{
+    private $constants = array();
+
+    public function __construct()
+    {
+
+    }
+
+    public function __destruct()
+    {
+
+    }
+
+    public function init()
+    {
+
+    }
+
+    public function hash_algos()
+    {
+
+    }
+
+    public function hash_copy($context)
+    {
+
+    }
+
+    public function hash_equals()
+    {
+
+    }
+
+    public function hash_file($algo, $filename)
+    {
+
+    }
+
+    public function hash_final($context)
+    {
+
+    }
+
+    public function hash_hkdf()
+    {
+
+    }
+
+    public function hash_hmac_algos()
+    {
+
+    }
+
+    public function hash_hmac_file($algo, $filename, $key)
+    {
+
+    }
+
+    public function hash_hmac($algo, $data, $key)
+    {
+
+    }
+
+    public function hash_init($algo)
+    {
+
+    }
+
+    public function hash_pbkdf2()
+    {
+
+    }
+
+    public function hash_update_file($context, $filename)
+    {
+
+    }
+
+    public function hash_update_stream($context, $handle)
+    {
+        
+    }
+
+    public function hash_update($context, $data)
+    {
+
+    }
+
+    
+
+    public function hash($algo, $data)
+    {
+
+    }
+}
