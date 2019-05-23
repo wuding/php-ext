9f158d7aded7977a399b394e634054c2f260b6a6
wuding <netjoin@gmail.com>
2019-05-23 17:55:11 +0800

    xin zeng yu ding yi bian liang lei 新  增   预 定   义 变   量    类

---
+++ src/Reserved/Variable.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Ext\Reserved;
+
+class Variable
+{
+	public function __construct()
+	{
+
+	}
+
+	public function __destruct()
+	{
+
+	}
+
+	public function init()
+	{
+
+	}
+
+	public function server()
+	{
+
+	}
+
+	public function env()
+	{
+
+	}
+
+	public function session()
+	{
+
+	}
+
+	public function cookie()
+	{
+
+	}
+
+	public function post()
+	{
+
+	}
+
+	public function get()
+	{
+
+	}
+
+	public function request()
+	{
+
+	}
+
+	public function files()
+	{
+
+	}
+
+	public function php_errormsg()
+	{
+
+	}
+
+	public function http_raw_post_data()
+	{
+
+	}
+
+	public function http_response_header()
+	{
+
+	}
+
+	public function argc()
+	{
+
+	}
+
+	public function argv()
+	{
+
+	}
+
+	public function globals()
+	{
+	
+	}
+}
