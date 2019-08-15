493e458b75513d38b5f2e850c26ea487f8aa69de
wuding <netjoin@gmail.com>
2019-08-15 20:32:02 +0800

    新增 Misc 和 MySQL

---
+++ src/Misc.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 其他杂项
+ */
+
+namespace Ext;
+
+class Misc
+{
+	public function __construct()
+	{
+
+	}
+
+	public function __destruct()
+	{
+
+	}
+
+	public function init()
+	{
+
+	}
+
+	public function define(string $name, mixed $value, bool $case_insensitive = false) : bool
+	{
+		
+	}
+
+	public function defined(string $name) : bool
+	{
+
+	}
+}
---
+++ src/MySQL.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ext;
+
+class MySQL
+{
+    public function __construct()
+    {
+
+    }
+
+    public function __destruct()
+    {
+
+    }
+
+    public function init()
+    {
+
+    }
+
+    /**
+     * 
+     */
+    public function mysql_pconnect(string $server, string $username, string $password, int $client_flags) : resource
+    {
+
+    }
+}
