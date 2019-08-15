24c1e985e506ba7edc42d83f399d2c1e458f58eb
wuding <netjoin@gmail.com>
2019-08-15 20:16:43 +0800

    修正缩进为空格

--- src/Reserved/Variable.php
+++ src/Reserved/Variable.php
@@ -4,88 +4,88 @@ namespace Ext\Reserved;
 
 class Variable
 {
-	public function __construct()
-	{
+    public function __construct()
+    {
 
-	}
+    }
 
-	public function __destruct()
-	{
+    public function __destruct()
+    {
 
-	}
+    }
 
-	public function init()
-	{
+    public function init()
+    {
 
-	}
+    }
 
-	public function server()
-	{
+    public function server()
+    {
 
-	}
+    }
 
-	public function env()
-	{
+    public function env()
+    {
 
-	}
+    }
 
-	public function session()
-	{
+    public function session()
+    {
 
-	}
+    }
 
-	public function cookie()
-	{
+    public function cookie()
+    {
 
-	}
+    }
 
-	public function post()
-	{
+    public function post()
+    {
 
-	}
+    }
 
-	public function get()
-	{
+    public function get()
+    {
 
-	}
+    }
 
-	public function request()
-	{
+    public function request()
+    {
 
-	}
+    }
 
-	public function files()
-	{
+    public function files()
+    {
 
-	}
+    }
 
-	public function php_errormsg()
-	{
+    public function php_errormsg()
+    {
 
-	}
+    }
 
-	public function http_raw_post_data()
-	{
+    public function http_raw_post_data()
+    {
 
-	}
+    }
 
-	public function http_response_header()
-	{
+    public function http_response_header()
+    {
 
-	}
+    }
 
-	public function argc()
-	{
+    public function argc()
+    {
 
-	}
+    }
 
-	public function argv()
-	{
+    public function argv()
+    {
 
-	}
+    }
 
-	public function globals()
-	{
-	
-	}
+    public function globals()
+    {
+        print_r(\Func\globals());
+    }
 }
