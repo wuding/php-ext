1d8f8a8a063aaf0eb8e31f95f86e40c39ab8d08d
netjoin <netjoin@gmail.com>
2019-09-13 15:05:31 +0800

    添加常量初始化

--- src/Mbstring.php
+++ src/Mbstring.php
@@ -7,9 +7,11 @@ class Mbstring
     public $str = null;
     public $string = null;
     public $from_encoding = null;
+    public static $constants_string = 'MB_OVERLOAD_MAIL|MB_OVERLOAD_STRING|MB_OVERLOAD_REGEX|MB_CASE_UPPER|MB_CASE_LOWER|MB_CASE_TITLE';
     
     public function __construct($str = null, $from_encoding = null, $to_encoding = null)
     {
+        parent::init();
         if ($str) {
             $this->init($str, $from_encoding, $to_encoding);
         }
@@ -40,5 +42,35 @@ class Mbstring
         
         $str = $this->str;
         return preg_replace($pattern, $replace, $str, 1);
+    }
+
+    public function checkEncoding($var = null, $encoding = mb_internal_encoding())
+    {
+        return mb_check_encoding($var, $encoding);
+    }
+
+    public function chr($cp, $encoding = null)
+    {
+        return mb_chr($cp, $encoding);
+    }
+
+    public function convertCase($str, $mode, $encoding = mb_internal_encoding())
+    {
+        return mb_convert_case($str, $mode, $encoding);
+    }
+
+    public function convertEncoding($str, $to_encoding, $from_encoding = mb_internal_encoding())
+    {
+        return mb_convert_encoding($str, $to_encoding, $from_encoding);
+    }
+
+    public function convertKana($str, $option = 'KV', $encoding = mb_internal_encoding())
+    {
+        return mb_convert_kana($str, $option, $encoding);
+    }
+
+    public function convertVariables($to_encoding, $from_encoding, &$vars)
+    {
+        return mb_convert_variables($to_encoding, $from_encoding, $vars);
     }
 }
--- src/_Abstract.php
+++ src/_Abstract.php
@@ -4,5 +4,14 @@ namespace Ext;
 
 class _Abstract
 {
+    public static $constants = array();
 
+    public function init()
+    {
+        $constants_array = explode('|', self::$constants_string);
+        foreach ($constants_array as $constant_name) {
+            eval("\$const = $constant_name;");
+            self::$constants[$constant_name] = $const;
+        }
+    }
 }
