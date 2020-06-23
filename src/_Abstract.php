a0999b41824675dc37c0c1ed1cc250ed382596cb
wuding <netjoin@gmail.com>
2020-06-24 01:10:46 +0800

    初始化常量键值对支持前缀，添加获取、设置句柄方法

--- src/_Abstract.php
+++ src/_Abstract.php
@@ -5,13 +5,31 @@ namespace Ext;
 class _Abstract
 {
     public static $constants = array();
+    public static $constPrefix = null;
+    public static $fp = null;
 
     public function init()
     {
-        $constants_array = explode('|', self::$constants_string);
-        foreach ($constants_array as $constant_name) {
-            eval("\$const = $constant_name;");
-            self::$constants[$constant_name] = $const;
+        $arr = preg_split('/[|,]+/', self::$constants_string);
+        foreach ($arr as $name) {
+            if (self::$constPrefix) {
+                $name = self::$constPrefix .'_'. $name;
+            }
+            self::$constants[$name] = constant($name);
         }
+        return self::$constants;
+    }
+
+    public static function getHandle($handle = null, $varName = 'fp')
+    {
+        return $handle ?: self::$$varName;
+    }
+
+    public static function setHandle($handle = null, $varName = 'fp')
+    {
+        if ($handle) {
+            self::$$varName = $handle;
+        }
+        return false;
     }
 }
