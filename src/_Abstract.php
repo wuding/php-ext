326065d968f0462cc9eba452f92cd121568be902
wuding <netjoin@gmail.com>
2020-09-27 23:04:55 +0800

    添加方法：预定义常量键值对、字符串转数组

--- src/_Abstract.php
+++ src/_Abstract.php
@@ -18,4 +18,32 @@ class _Abstract
             }
         }
     }
+
+    // 预定义常量键值对
+    public static function _const()
+    {
+        $variable  = static::_strToArr(static::$constStr);
+        $arr = [];
+        foreach ($variable as $name) {
+            $arr[$name] = constant($name);
+        }
+        return static::$constants = $arr;
+    }
+
+    // 字符串转数组
+    public static function _strToArr($string = null, $join = '_', $delimiter = ';', $assign = '=', $separator = ',')
+    {
+        $arr = [];
+        $variable = explode($delimiter, $string);
+        foreach ($variable as $value) {
+            $val = explode($assign, $value);
+            $prefix = $val[0];
+            $str = $val[1];
+            $suffixes = explode($separator, $str);
+            foreach ($suffixes as $suffix) {
+                $arr[] = "$prefix$join$suffix";
+            }
+        }
+        return $arr;
+    }
 }
