a96e4b0e2223e192485c6a4787dee88f5fe1e8a6
wuding <netjoin@gmail.com>
2020-10-18 00:21:16 +0800

    添加方法：配置、合并静态属性，自定义参数缺省值

--- src/_Abstract.php
+++ src/_Abstract.php
@@ -7,6 +7,8 @@ class _Abstract
     public static $constants = null;
     public static $constStr = null;
     public static $ini = null;
+    public static $defaultValue = null;
+    public static $args = array();
 
     public function __construct()
     {
@@ -36,12 +38,66 @@ class _Abstract
         $arr = [];
         $variable = explode($delimiter, $string);
         foreach ($variable as $value) {
+            if (!$value) {
+                continue 1;
+            }
             $val = explode($assign, $value);
             $prefix = $val[0];
             $str = $val[1];
             $suffixes = explode($separator, $str);
             foreach ($suffixes as $suffix) {
                 $arr[] = "$prefix$join$suffix";
+            }
+        }
+        return $arr;
+    }
+
+    // 配置静态属性
+    public static function config($variable = null, $item = 'args', $skipNull = true)
+    {
+        $item = null === $item ? 'args' : $item;
+
+        foreach ($variable as $key => $value) {
+            if (is_array($value)) {
+                foreach ($value as $k => $v) {
+                    if (null === $v && $skipNull) {
+                        continue 1;
+                    }
+                    static::$$item[$key][$k] = $v;
+                }
+                continue 1;
+            }
+            static::$$item[$key] = $value;
+        }
+        return static::$$item;
+    }
+
+    // 参数值覆盖默认值
+    public static function args($item = null, $variable = [], $skipNull = true)
+    {
+        $args = self::mergeVars('args');
+        $arr = $args[$item] ?? [];
+        foreach ($variable as $key => $value) {
+            if (null === $value && $skipNull) {
+                continue 1;
+            }
+            $arr[$key] = $value;
+        }
+        return $arr;
+    }
+
+    // 合并静态属性
+    public static function mergeVars($varname = null)
+    {
+        $arr = self::$$varname ?? [];
+        $static = static::$$varname ?? [];
+        foreach ($static as $key => $value) {
+            if (!array_key_exists($key, $arr)) {
+                $arr[$key] = $value;
+            } else {
+                foreach ($value as $k => $v) {
+                    $arr[$key][$k] = $v;
+                }
             }
         }
         return $arr;
