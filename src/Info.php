6f967ce2e126bbfe8d2c92b24083f0fb000d1869
wuding <netjoin@gmail.com>
2021-08-10 17:21:19 +0800

    添加获取定义的常量方法

--- src/Info.php
+++ src/Info.php
@@ -4,7 +4,7 @@ namespace Ext;
 
 class Info extends _Abstract
 {
-    const VERSION = 20.2680870;
+    const VERSION = 20.5871179;
 
     public static $constants = [];
     public static $constStr = 'ASSERT=ACTIVE,CALLBACK,BALL,WARNING,QUIET_EVAL;';
@@ -18,5 +18,28 @@ class Info extends _Abstract
     public static function assertOptions($what = null, $value = null)
     {
         return assert_options($what, $value);
+    }
+
+    /*
+    +---------------------------------------------+
+    + 运行时
+    +---------------------------------------------+
+    */
+
+    public static function getDefinedConstants($categorize = false, $cat_name = null)
+    {
+        $constants = get_defined_constants($categorize);
+        if (!$cat_name) {
+            return $constants;
+        }
+
+        $cat_nm = strtolower($cat_name);
+        foreach ($constants as $key => $value) {
+            $keyname = strtolower($key);
+            if ($cat_nm === $keyname) {
+                return $value;
+            }
+        }
+        return false;
     }
 }
