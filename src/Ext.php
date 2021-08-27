7ebf193b1a1d3391b11ca45b02c8682e780c54eb
wuding <netjoin@gmail.com>
2021-08-27 15:35:42 +0800

    加入上一版本类和函数

--- src/Ext.php
+++ src/Ext.php
@@ -1,1 +1,25 @@
 <?php
+
+class Ext
+{
+    const VERSION = '20.213.92';
+}
+
+function ext($library)
+{
+    $arr = is_string($library) ? [$library] : $library;
+    $prefix = (PHP_SHLIB_SUFFIX === 'dll') ? 'php_' : '';
+    $suffix = '.' . PHP_SHLIB_SUFFIX;
+    $result = [];
+    foreach ($arr as $name) {
+        if (!extension_loaded($name)) {
+            if (!function_exists('dl')) {
+                print_r([__FILE__, __LINE__, "Call to undefined function dl($name)"]);
+                exit;
+            }
+            $basename = $prefix . $name . $suffix;
+            $result[$name] = dl($basename);
+        }
+    }
+    return $result;
+}
