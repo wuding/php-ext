9e583ebf9eec8d2f05921995b4f5ac68b0acb976
wuding <netjoin@gmail.com>
2022-03-21 16:31:47 +0800

    新增命名空间类

---
+++ src/Lang/Namespace/Namespaces.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ext\Lang\Namespace;
+
+class Namespaces
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'Namespaces overview',
+        'Defining namespaces',
+        'Declaring sub-namespaces',
+        'Defining multiple namespaces int the same file',
+        'Using namespaces: Basics',
+        'Namespaces and dynamic language features',
+        'namespace keyword and __NAMESPACE__ constant',
+        'Using namespaces: Aliasing/Imporing',
+        'Global space',
+        'Using namespaces: fallback to global function/constant',
+        'Name resolution rules',
+        'FAQ: things you need to know about namespaces',
+    );
+}
