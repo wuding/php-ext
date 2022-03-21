f69c414710750ef260cd0fd5c07f8ab15ad485d1
wuding <netjoin@gmail.com>
2022-03-21 17:29:07 +0800

    新增异常类

---
+++ src/Lang/Exception/Exceptions.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ext\Lang\Exception;
+
+class Exceptions
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'Extending Exceptions',
+    );
+
+    public static trying()
+    {
+
+    }
+
+    public static catched()
+    {
+
+    }
+
+    public static finals()
+    {
+
+    }
+
+    public static function setExceptionHandler()
+    {
+
+    }
+}
