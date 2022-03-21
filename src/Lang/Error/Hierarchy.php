d81bc5a1d295eec6a8294b64e2214bc9da9c58c2
wuding <netjoin@gmail.com>
2022-03-21 17:10:15 +0800

    新增错误层次结构类

---
+++ src/Lang/Error/Hierarchy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ext\Lang\Error;
+
+class Hierarchy
+{
+    const VERSION = '22.3.21';
+
+    public $error_hierarchy = array(
+        'Throwable' => array(
+            'Error' => array(
+                'ArithmericError' => array(
+                    'DivisionByZeroError',
+                ),
+                'AssertionError',
+                'CompileError' => array(
+                    'ParseError',
+                ),
+                'TypeError' => array(
+                    'ArgumentCountError',
+                ),
+                'ValueError',
+                'UnhandledMatchError',
+                'FiberError',
+            ),
+            'Exception',
+        ),
+    );
+}
