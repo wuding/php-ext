5eebd33188841ea5f9657fcd6d85df7fcfbf33b0
wuding <netjoin@gmail.com>
2022-03-21 18:22:04 +0800

    新增预定义接口类

---
+++ src/Lang/Predefined/Interface/PredefinedInterfacesAndClasses.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ext\Lang\Predefined\Interface;
+
+class PredefinedInterfacesAndClasses
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'Traversable',
+        'Iterator',
+        'IteratorAggregate',
+        'Throwable',
+        'ArrayAccess',
+        'Serializable',
+        'Closure',
+        'Generator',
+        'Fiber',
+        'WeakReference',
+        'WeakMap',
+        'Stringable',
+        'UnitEnum',,
+        'BackedEnum',
+    );
+}
