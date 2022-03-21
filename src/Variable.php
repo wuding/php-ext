726b39a76c58fcf77fd3954d40a450e9c8ca4525
wuding <netjoin@gmail.com>
2022-03-21 19:54:55 +0800

    新增变量处理类

---
+++ src/Variable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ext;
+
+class Variable
+{
+    const VERSION = '22.3.21';
+
+    /*
+    +---------------------------------------------------------------+
+    + 打印信息表示
+    +---------------------------------------------------------------+
+    */
+
+    public static function dump()
+    {
+
+    }
+
+    public static function export()
+    {
+
+    }
+
+    public static function printReadable()
+    {
+
+    }
+
+    public static function debugZvalDump()
+    {
+
+    }
+}
