6b85794d1b5c6b213b00afbddd141ba485a2c273
wuding <netjoin@gmail.com>
2022-03-21 17:39:49 +0800

    新增生成器类

---
+++ src/Lang/Generator/Generators.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ext\Lang\Generator;
+
+class Generators
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'Generators overview',
+        'Generator syntax',
+        'Comparing generators with Iterator objects',
+    );
+}
