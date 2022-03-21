a6951e32e45c26f144335b8f7a2486f469149e3c
wuding <netjoin@gmail.com>
2022-03-21 12:56:51 +0800

    新增函数类

---
+++ src/Lang/Function/Functions.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ext\Lang\Function;
+
+class Functions
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'User-defined functins',
+        'Function arguments',
+        'Returning values',
+        'Variable functions',
+        'Internal (built-in) functions',
+        'Anonymous functions',
+        'Arrow Functions',
+        'First class callable syntax',
+    );
+}
