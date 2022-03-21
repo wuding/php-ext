cc2e6169e435da8bc6caab36d3caf50bf7029737
wuding <netjoin@gmail.com>
2022-03-21 11:43:43 +0800

    新增常量目录类

---
+++ src/Lang/Constant/TableOfContents.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ext\Lang\Constant;
+
+class TableOfContents
+{
+    const VERSION = '22.3.21';
+
+    public $regular_expression = "";
+
+    public static function define()
+    {
+
+    }
+
+    public static function constant($name)
+    {
+
+    }
+}
