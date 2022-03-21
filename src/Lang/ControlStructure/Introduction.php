432233601ca8405d0d6ad3ccad94790f78accb7e
wuding <netjoin@gmail.com>
2022-03-21 12:38:57 +0800

    新增流程控制简介类

---
+++ src/Lang/ControlStructure/Introduction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ext\Lang\ControlStructure;
+
+class Introduction
+{
+    const VERSION = '22.3.21';
+
+    public $see_also = array(
+        'list()',
+        'array()',
+        'echo',
+        'eval()',
+        'print',
+    );
+}
