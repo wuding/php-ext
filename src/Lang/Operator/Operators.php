084a0173f6ea4f418af314908e2f04bfd5587a85
wuding <netjoin@gmail.com>
2022-03-21 12:19:18 +0800

    新增运算符类

---
+++ src/Lang/Operator/Operators.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ext\Lang\Operator;
+
+class Operators
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'Operator Precedence',
+        'Arithmetic Operators',
+        'Assignment Operators',
+        'Bitwise Operators',
+        'Comparison Operators',
+        'Error Control Operators',
+        'Execution Operators',
+        'Incrementing/Decrementing Operators',
+        'Logical Operators',
+        'String Operators',
+        'Array Operators',
+        'Type Operators',
+    );
+}
