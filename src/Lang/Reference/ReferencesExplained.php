53d287c52321d677095ec8bacd20c909709d65b5
wuding <netjoin@gmail.com>
2022-03-21 17:49:52 +0800

    新增引用的注解类

---
+++ src/Lang/Reference/ReferencesExplained.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ext\Lang\Reference;
+
+class ReferencesExplained
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        "What References Are",
+        'What References Do',
+        'What References Are Not',
+        'Passing by Reference',
+        'Returning References',
+        'Unsetting References',
+        'Spotting References',
+    );
+}
