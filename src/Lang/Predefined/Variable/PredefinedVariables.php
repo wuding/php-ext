a98cbfdb7be0c1b380365b11f29693680e0694f9
wuding <netjoin@gmail.com>
2022-03-21 17:57:01 +0800

    新增预定义变量类

---
+++ src/Lang/Predefined/Variable/PredefinedVariables.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Ext\Lang\Predefined\Variable;
+
+class PredefinedVariables
+{
+    const VERSION = '22.3.21';
+
+    public $table_of_contents = array(
+        'Superglobals',
+        '$GLOBALS',
+        '$_SERVER',
+        '$_GET',
+        '$_POST',
+        '$_FILES',
+        '$_REQUEST',
+        "$_SESSION",
+        '$_ENV',
+        '$_COOKIE',
+        '$php_errormsg',
+        '$http_response_header',
+        '$argc',
+        '$argv',
+    );
+}
