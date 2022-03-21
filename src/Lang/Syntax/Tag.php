15a0c0987a235ff96dca06f5f9467cbbb014c54c
wuding <netjoin@gmail.com>
2022-03-21 10:43:58 +0800

    新增 PHP 标记类

---
+++ src/Lang/Syntax/Tag.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ext;
+
+class Tag
+{
+    const VERSION = '22.3.21';
+
+    public $built = array(
+        '--disable-short-tags' => null,
+    );
+
+    public $ini = array(
+        'short_open_tag' => true,
+    );
+
+    // 开始和结束标记
+    public static function openingAndClosingTags()
+    {
+
+    }
+
+    // echo 标记简写
+    public static function shortEchoTag()
+    {
+
+    }
+
+    // 短标记
+    public static function shortTags()
+    {
+
+    }
+
+    // 仅包含 PHP 代码
+    public static function onlyPhpCode()
+    {
+
+    }
+}
