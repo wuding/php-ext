8ef99e2b7c3330c0ffe55019e482e976f7197e83
wuding <netjoin@gmail.com>
2022-03-23 13:09:55 +0800

    添加函数返回值、参数类属性，增加参数和 Coding 实现方法； 添加打印信息表示调试方法切换方法、及获取函数方法参数名称预定义方法

--- src/Variable.php
+++ src/Variable.php
@@ -6,6 +6,38 @@ class Variable
 {
     const VERSION = '22.3.21';
 
+    public static $return_values = array(
+        'var_export' => array(
+            'Returns the variable representation' => true,
+            null => 'Otherwise',
+        ),
+
+    );
+
+    public static $parameters = array(
+        'pRvDvEdZvalD' => array(
+            'func_n' => array(
+                0 => 'dump',
+                1 => 'export',
+                2 => 'printReadable',
+                3 => 'debugZvalDump',
+                'var_dump' => 'dump',
+                'var_export' => 'export',
+                'print_r' => 'printReadable',
+                'debug_zval_dump' => 'debugZvalDump',
+            ),
+        ),
+    );
+
+    public static function _function_name($var)
+    {
+        $return_values = $var;
+        if (is_numeric($var) || preg_match("/_/", $var)) {
+            $return_values = self::$parameters['pRvDvEdZvalD']['func_n'][$var] ?? 0;
+        }
+        return $return_values;
+    }
+
     /*
     +---------------------------------------------------------------+
     + 打印信息表示
@@ -14,21 +46,36 @@ class Variable
 
     public static function dump()
     {
-
+        $function = 'var_dump';
+        $param_arr = func_get_args();
+        $return_values = call_user_func_array($function, $param_arr);
+        return $return_values;
     }
 
-    public static function export()
+    public static function export($value, bool $return = false): ?string
     {
-
+        $return_values = var_export($value, $return);
+        return $return_values;
     }
 
-    public static function printReadable()
+    public static function printReadable($value, bool $return = false)
     {
-
+        $return_values = print_r($value, $return);
+        return $return_values;
     }
 
     public static function debugZvalDump()
     {
+        $function = 'debug_zval_dump';
+        $param_arr = func_get_args();
+        $return_values = call_user_func_array($function, $param_arr);
+        return $return_values;
+    }
 
+    public static function pRvDvEdZvalD($func_n = 0, $param_arr)
+    {
+        $function = self::_function_name($func_n);
+        $return_values = call_user_func_array($function, $param_arr);
+        return $return_values;
     }
 }
