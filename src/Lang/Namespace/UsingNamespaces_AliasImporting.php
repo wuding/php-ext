4dc0f400dc54a7e9ba0d5f346e2ab3ffebddabd4
wuding <netjoin@gmail.com>
2022-03-23 11:23:53 +0800

    新增使用命名空间别名/导入类

---
+++ src/Lang/Namespace/UsingNamespaces_AliasImporting.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ext\Lang\Namespace;
+
+class UsingNamespaces_AliasImporting
+{
+    const VERSION = '22.3.23';
+
+    public static $para = array(
+        'can_alias_import' => array(
+            'constants', 'functions', 'classes', 'interfaces', 'namespaces',
+        ),
+        'importing_kinds' => array(
+            'importing/aliasing with the use operator',
+            'importing/aliasing with the use operator, multiple use statements combined',
+            'importing and dynamic names',
+            'importing and fully qualified names',
+        ),
+        'scoping_rules_for_importing' => array(
+            'Illegal importing rule',
+            ''
+        ),
+        'group_use_declarations' => array(
+
+        ),
+    );
+}
