332c75b9e32c880c70a54914413bef12e9e5bb76
wuding <netjoin@gmail.com>
2022-03-23 12:39:54 +0800

    新增错误控制运算符类

---
+++ src/Lang/Operator/ErrorControlOperators.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ext\Lang\Operator;
+
+class ErrorControlOperators
+{
+    const VERSION = '22.3.23';
+
+    public static $para = array(
+        'The @-operator works only on expressions',
+    );
+}
