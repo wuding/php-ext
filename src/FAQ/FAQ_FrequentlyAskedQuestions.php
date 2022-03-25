4941a22624261e9d777df888a507a50f7caa9dec
wuding <netjoin@gmail.com>
2022-03-25 22:26:00 +0800

    新增常见问题扩展类

---
+++ src/FAQ/FAQ_FrequentlyAskedQuestions.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ext\FAQ;
+
+class FAQ_FrequentlyAskedQuestions
+{
+    const VERSION = '22.3.25';
+
+    public $chunklist = array(
+        'General Information',
+        'Mailing lists',
+        'Obtaining PHP',
+        'Database issues',
+        'Installation',
+        'Build Problems',
+        'Using PHP',
+        'Password Hashing' => 'Safe Password Hashing',
+        'PHP and HTML',
+
+        'PHP and COM',
+        'Miscellaneous Questions',
+    );
+}
