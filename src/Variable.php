070bed61e78cc69253775d027570055bf61cb16b
wuding <netjoin@gmail.com>
2022-03-25 23:36:49 +0800

    添加检测是否为空值函数方法，添加变量类在线文档页面标题结构值

--- src/Variable.php
+++ src/Variable.php
@@ -29,6 +29,43 @@ class Variable
         ),
     );
 
+    public $pages = array(
+        'refman' => array(
+            'is_null' => array(
+                'verinfo' => array('>=' => '4.0.4', 5, 7, 8),
+                'purpose' => 'Finds whether a variable is null',
+                'para' => array(
+                    'Description' => array(
+                        'synopsis' => array(
+                            'is_null(mixed $value): bool',
+                        ),
+                    ),
+                    'Parameters' => array(
+                        'value' => null,
+                    ),
+                    'Return Values' => array(
+                        true => 'if value is null',
+                        false => 'otherwise',
+                    ),
+                    'Examples' => array(
+                        'is_null() example',
+                    ),
+                    'See Also' => array(
+                        'The null type',
+                        'isset()',
+                        'is_bool()',
+                        'is_numeric()',
+                        'is_float()',
+                        'is_int()',
+                        'is_string()',
+                        'is_object()',
+                        'is_array()',
+                    ),
+                ),
+            ),
+        ),
+    );
+
     public static function _function_name($var)
     {
         $return_values = $var;
@@ -76,6 +113,19 @@ class Variable
     {
         $function = self::_function_name($func_n);
         $return_values = call_user_func_array($function, $param_arr);
+        return $return_values;
+    }
+
+
+    /*
+    +---------------------------------------------------------------+
+    + 检测
+    +---------------------------------------------------------------+
+    */
+
+    public static function isNull($value, $options = null)
+    {
+        $return_values = is_null($value);
         return $return_values;
     }
 }
