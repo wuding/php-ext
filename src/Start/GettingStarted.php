eb549732f5832b6495df899872934cc112e5b9df
wuding <netjoin@gmail.com>
2022-03-25 22:12:09 +0800

    新增入门指引扩展类

---
+++ src/Start/GettingStarted.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ext\Start;
+
+class GettingStarted
+{
+    const VERSION = '22.3.25';
+
+    public $chunklist = array(
+        'Introduction' => array(
+            'What is PHP?',
+            'What can PHP do?',
+        ),
+        'A simple tutorial' => array(
+            'What do I need?',
+            'Your first PHP-enabled page',
+            'Something Useful',
+            'Dealing with Forms',
+            "What's next?",
+        ),
+    );
+}
