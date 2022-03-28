1a886119b28915dcd32f80d9580de8b2527eec41
wuding <netjoin@gmail.com>
2022-03-28 22:14:18 +0800

    添加页面段落，添加 Ds\Map::xor 数组参数调用方法

--- src/Ds.php
+++ src/Ds.php
@@ -6,6 +6,36 @@ class Ds
 {
     const VERSION = '22.3.28';
 
+    public $pages = array(
+        'refman' => array(
+            'Ds\Map::xor' => array(
+                'verinfo' => array(
+                    'PECL ds' => array(
+                        '>=' => '1.0.0',
+                    ),
+                ),
+                'purpose' => 'Creates a new map using keys of either the current instance or of another map, but not of both',
+                'para' => array(
+                    'Description' => array(
+                        'synopsis' => 'public Ds\Map::xor(Ds\Map $map): Ds\Map',
+                    ),
+                    'Parameters' => array(
+                        'map' => null,
+                    ),
+                    'Return Values' => array(
+                        'object' => 'Ds\Map',
+                    ),
+                    'See Also' => array(
+                        'Symmetric Difference',
+                    ),
+                    'Examples' => array(
+                        'Ds\Map::xor() example',
+                    ),
+                ),
+            ),
+        ),
+    );
+
     /*
     +---------------------------------------------------------------+
     + Interface
@@ -25,6 +55,19 @@ class Ds
     # Vector
     # Deque
     # Map
+
+    public static function xor($map = null, $variable = null)
+    {
+        $arr = array();
+        foreach ($variable as $key => $value) {
+            $result = new \Ds\Map($value);
+            $arr[] = $result;
+        }
+
+        $return_values = $arr[0]->xor($arr[1]);
+        return $return_values;
+    }
+
     # Pair
     # Set
     # Stack
