7a41de0448ac548bdbf701582492e70e189fc2ed
wuding <netjoin@gmail.com>
2022-03-29 16:29:49 +0800

    添加布尔返回值函数 g 索引，并更新版本号日期

--- src/Manual/layout/content/ReturnValues/scalar/Bool.php
+++ src/Manual/layout/content/ReturnValues/scalar/Bool.php
@@ -4,5 +4,11 @@ namespace Ext\Manual\layout\content\ReturnValues\scalar;
 
 class Bool
 {
-    const VERSION = '22.3.26';
+    const VERSION = '22.3.28';
+
+    public $function = array(
+        'g' => array(
+            'gnupg_cleardecryptkeys',
+        ),
+    );
 }
