db5ee3bf3bb454eaf1d8ea2ba556e1c69cde7d6d
wuding <netjoin@gmail.com>
2022-04-01 18:59:21 +0800

    新增返回值类型字符串函数集

---
+++ src/Manual/layout/content/ReturnValues/scalar/String.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ext\Manual\layout\content\ReturnValues\scalar;
+
+class String
+{
+    const VERSION = '22.4.1';
+
+    public $function = array(
+        'd' => array(
+            'date' => false,
+        ),
+    );
+}
