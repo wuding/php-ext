4facf086b44bc8fcd36bc9cbe2b42e2459efa95b
wuding <netjoin@gmail.com>
2022-04-02 13:09:25 +0800

    添加移出开头单元函数方法、页面段落

--- src/Arr.php
+++ src/Arr.php
@@ -4,7 +4,41 @@ namespace Ext;
 
 class Arr extends _Abstract
 {
-    const VERSION = '21.8.10';
+    const VERSION = '22.4.2';
+
+    public static $predefined_constants = array(
+        /* array_change_key_case() */
+        'CASE_LOWER',
+        'CASE_UPPER',
+
+        /* array_multisort() */
+        'SORT_ASC',
+        'SORT_DESC',
+
+        /* Sorting type flags */
+        'SORT_REGULAR',
+        'SORT_NUMERIC'
+        'SORT_STRING',
+        'SORT_LOCALE_STRING',
+        'SORT_NATURAL',
+        'SORT_FLAG_CASE',
+
+        /* Filter flags: array_filter() */
+        'ARRAY_FILTER_USE_KEY',
+        'ARRAY_FILTER_USE_BOTH',
+
+        'COUNT_NORAML',
+        'COUNT_RECURSIVE',
+
+        'EXTR_OVERWRITE',
+        'EXTR_SKIP',
+        'EXTR_PREFIX_SAME',
+        'EXTR_PREFIX_ALL',
+        'EXTR_PREFIX_INVALID',
+        'EXTR_PREFIX_IF_EXISTS',
+        'EXTR_IF_EXISTS',
+        'EXTR_REFS',
+    );
 
     // 通过键名获取交集
     public static function intersect($var = null, $variable = array())
@@ -39,5 +73,18 @@ class Arr extends _Abstract
             $array[$key] = $val;
         }
         return $array;
+    }
+
+
+    /*
+    +---------------------------------------------------------------+
+    + 创建改变合并数组
+    +---------------------------------------------------------------+
+    */
+
+    public static function shift(&$array)
+    {
+        $return_values = array_shift($array);
+        return $return_values;
     }
 }
