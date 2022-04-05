06bc31e32323e304f29579fb0711b2badec73364
wuding <netjoin@gmail.com>
2022-04-05 12:34:08 +0800

    新增返回值类型数组函数集

---
+++ src/Manual/layout/content/ReturnValues/compound/Array.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ext\Manual\layout\content\ReturnValues\compound;
+
+class Array
+{
+    const VERSION = '22.4.5';
+
+    public $function = array(
+        'p' => array(
+            'pathinfo' => 'string',
+        ),
+    );
+}
