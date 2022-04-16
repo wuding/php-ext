1f65b269dbffd1df130d9224ee393ebe08e018bc
wuding <netjoin@gmail.com>
2022-04-16 21:21:48 +0800

    新增执行运算符

---
+++ src/Lang/Operator/ExecutionOperators.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ext\Lang\Operator;
+
+class ExecutionOperators
+{
+    const VERSION = '22.4.16';
+
+    public static $para = array(
+        'Execution Operators' => array(
+            'PHP supports on execution operator: backticks(``)',
+        ),
+    );
+}
