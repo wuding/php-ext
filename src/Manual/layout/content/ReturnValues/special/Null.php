12e76ecd9c05e4e9deda9fbae82c79f9ad4c95f0
wuding <netjoin@gmail.com>
2022-04-16 20:58:30 +0800

    新增返回值类型空函数集

---
+++ src/Manual/layout/content/ReturnValues/special/Null.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ext\Manual\layout\content\ReturnValues\special;
+
+class Null
+{
+    const VERSION = '22.4.16';
+
+    public $function = array(
+        'a' => array(
+            'array_shift' => 'Returns the shifted value',
+        ),
+    );
+}
