7abe79b78bece8780204fcb8210e4364f8a901ae
wuding <netjoin@gmail.com>
2022-04-16 20:47:46 +0800

    运算符附加中文翻译

--- src/Lang/Operator/Operators.php
+++ src/Lang/Operator/Operators.php
@@ -4,20 +4,19 @@ namespace Ext\Lang\Operator;
 
 class Operators
 {
-    const VERSION = '22.3.21';
+    const VERSION = '22.4.16';
 
     public $table_of_contents = array(
-        'Operator Precedence',
-        'Arithmetic Operators',
-        'Assignment Operators',
-        'Bitwise Operators',
-        'Comparison Operators',
-        'Error Control Operators',
-        'Execution Operators',
-        'Incrementing/Decrementing Operators',
-        'Logical Operators',
-        'String Operators',
-        'Array Operators',
-        'Type Operators',
+        'Operator Precedence' => '运算符优先级',
+        'Arithmetic Operators' => '算术运算符',
+        'Assignment Operators' => '赋值运算符',
+        'Bitwise Operators' => '位运算符',
+        'Comparison Operators' => '比较运算符',
+        'Error Control Operators' => '错误控制运算符',
+        'Execution Operators' => '执行运算符',
+        'Incrementing/Decrementing Operators' => '递增/递减运算符',
+        'Logical Operators' => '逻辑运算符',
+        'String Operators' => '字符串运算符',
+        'Array Operators' => '类型运算符',
     );
 }
