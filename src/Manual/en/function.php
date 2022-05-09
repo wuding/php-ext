8d3b08f25a0e03970f07fa0638b23c772715d6ff
wuding <netjoin@gmail.com>
2022-05-09 10:26:30 +0800

    新增 PHP 手册函数页面类

---
+++ src/Manual/en/function.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ext\Manual\en;
+
+class function
+{
+    const VERSION = '22.5.9';
+
+    public $pages = array(
+        'refman' => array(
+            'passthru' => array(
+                'verinfo' => array(4, 5, 7, 8),
+                'purpose' => 'Execute an external program and display raw output',
+                'para' => array(
+                    'Description' => array(
+                        'synopsis' => array(
+                            'passthru(string $command, int &$result_code = null): ?bool',
+                        ),
+                    ),
+                    'Parameters' => array(
+                        'command' => null,
+                        'result_code' => null,
+                    ),
+                    'Return Values' => array(
+                        null => 'on success',
+                        false => 'failure',
+                    ),
+                    'Notes' => array(
+                    ),
+                    'See Also' => array(
+                        'exec()' => 'Execute an external program',
+                        'system()' => 'Execute an external program and display the output',
+                        'popen()' => 'Opens process file pointer',
+                        'escapeshellcmd()' => 'Escape shell metacharacters',
+                        'backtrick operator'
+                    ),
+                ),
+            ),
+        ),
+    );
+}
