d9c03ce6c52e8fcded532b3c71115d18531c5547
wuding <netjoin@gmail.com>
2022-05-09 23:34:40 +0800

    添加 autoload 和 原始函数重命名

--- example/exec/passthru.php
+++ example/exec/passthru.php
@@ -1,10 +1,39 @@
 <?php
+define('ROOT', dirname(__DIR__, 5));
+
+$autoload = require ROOT ."/vendor/autoload.php";
+
+use function php\func\get;
+
+/*$arr = array('shutdown', 10, 16, 36, 62, 2);
+$first = \Ext\Arr::shift($arr);
+
+var_export($first);
+var_export($arr);
+exit;*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*****************************************************************************/
 # 20
 
 /**/
 function ob_passthru() {
     ob_start();
-    passthru("<i>command</i>");
+    pass_through("<i>command</i>");
     $var = ob_get_contents();
     ob_end_clean();
 
@@ -23,7 +52,7 @@ function ob_passthru() {
 /**/
 function echo_passthru() {
     $PATH = __FILE__;
-    passthru ("echo $PATH");
+    pass_through ("echo $PATH");
 }
 
 
@@ -45,7 +74,7 @@ function attachment_passthru() {
     header("Content-Type: application/octet-stream");
     header("Content-Disposition: attachment; filename=\"myfile.zip\"");
     header("Content-Length: 11111");
-    passthru("cat myfile.zip",$err);
+    pass_through("cat myfile.zip",$err);
 }
 
 
@@ -61,3 +90,28 @@ function attachment_passthru() {
 //---------------------------------------------------------------------------//
 $pre = $_GET['pre'] ?? 'ob';
 eval("{$pre}_passthru();");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*****************************************************************************/
+#
+
+/**/
+function pass_through(string $command, int &$result_code = null) {
+    #print_r(get_defined_vars());
+    $result_values = \Ext\Exec::passthru($command, $result_code);
+    #var_export(get_defined_vars());
+}
