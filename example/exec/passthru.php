4a9a903b079021f1841a5d635c37125fa50284c9
wuding <netjoin@gmail.com>
2022-05-09 11:37:29 +0800

    新增 passthru 函数示例代码

---
+++ example/exec/passthru.php
@@ -0,0 +1,60 @@
+<?php
+# 20
+
+/*
+ob_start();
+passthru("<i>command</i>");
+$var = ob_get_contents();
+ob_end_clean();
+
+
+var_export($var);
+*/
+
+
+
+
+
+
+
+/******************************************************************************/
+# 6
+
+/*
+$PATH = __FILE__;
+passthru ("echo $PATH");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************/
+# 8
+
+/*
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=\"myfile.zip\"");
+header("Content-Length: 11111");
+passthru("cat myfile.zip",$err);
+*/
+
+
+
+
+
+
+
+
+
+
+
+/******************************************************************************/
