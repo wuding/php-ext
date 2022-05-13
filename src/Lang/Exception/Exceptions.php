1182fc36956b155d59886d6a4ab146b3c3e9fd4b
wuding <netjoin@gmail.com>
2022-05-13 21:00:54 +0800

    更新语言参考异常扩展类版本号、增加注释到方法

--- src/Lang/Exception/Exceptions.php
+++ src/Lang/Exception/Exceptions.php
@@ -4,7 +4,7 @@ namespace Ext\Lang\Exception;
 
 class Exceptions
 {
-    const VERSION = '22.3.21';
+    const VERSION = '22.5.13';
 
     public $table_of_contents = array(
         'Extending Exceptions',
@@ -25,6 +25,7 @@ class Exceptions
 
     }
 
+    // Global exception handler
     public static function setExceptionHandler()
     {
 
