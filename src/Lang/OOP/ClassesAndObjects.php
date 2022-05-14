2e4782f839bf95dec11fb3e0f3e64e3688b92fb9
wuding <netjoin@gmail.com>
2022-05-14 17:21:35 +0800

    更新类与对象版本号，添加手册重载页面地址、属性、方法

--- src/Lang/OOP/ClassesAndObjects.php
+++ src/Lang/OOP/ClassesAndObjects.php
@@ -4,7 +4,7 @@ namespace Ext\Lang\OOP;
 
 class ClassesAndObjects
 {
-    const VERSION = '22.3.21';
+    const VERSION = '22.5.14';
 
     public $table_of_contents = array(
         'Introduction',
@@ -33,4 +33,52 @@ class ClassesAndObjects
         'Covariance and Contravariance',
         'OOP Changelog',
     );
+
+    public $pages = array(
+        'https://www.php.net/manual/en/language.oop5.overloading.php',
+    );
+
+
+    /*
+    +---------------------------------------------------------------+
+    + Property overloading
+    +---------------------------------------------------------------+
+    */
+
+    public __set(string $name, mixed $value): void
+    {
+
+    }
+
+    public __get(string $name): mixed
+    {
+
+    }
+
+    public __isset(string $name): bool
+    {
+
+    }
+
+    public __unset(string $name): void
+    {
+
+    }
+
+
+    /*
+    +---------------------------------------------------------------+
+    + Method overloading
+    +---------------------------------------------------------------+
+    */
+
+    public __call(string $name, array $arguments): mixed
+    {
+
+    }
+
+    public static __callStatic(string $name, array $arguments): mixed
+    {
+
+    }
 }
