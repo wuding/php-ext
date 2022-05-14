d82c6db4541bd9b76ad224a4cb58f30a9a0a235f
wuding <netjoin@gmail.com>
2022-05-14 20:37:02 +0800

    更新 Json 版本号，添加 Unicode 编码、解码方法

--- src/Json.php
+++ src/Json.php
@@ -4,5 +4,53 @@ namespace Ext;
 
 class Json
 {
-    const VERSION = '22.5.13';
+    const VERSION = '22.5.14';
+
+    public function __construct()
+    {
+
+    }
+
+    /*
+    +---------------------------------------------------------------+
+    + Unicode
+    +---------------------------------------------------------------+
+    */
+
+    /**
+     * Unicode 解码
+     */
+    public static function unicodeDecode($str)
+    {
+        $json = "{str:\"$str\"}";
+        $obj = json_decode($json);
+        if (!$obj) {
+            print_r(['json decode is null', $obj, $str, __FILE__, __LINE__]);
+            exit;
+        }
+        return $obj->str;
+    }
+
+    /**
+     * Unicode 编码
+     */
+    public static function unicodeEncode($subject)
+    {
+        $in_charset = 'UTF-8';
+        $out_charset = 'UCS-4';
+        $frombase = 16;
+        $tobase = 10;
+        $pieces = [];
+        $preg = preg_match_all('/./u', $subject, $matches);
+        foreach ($matches[0] as $char) {
+            $str = iconv($in_charset, $out_charset, $char);
+            $number = bin2hex($str);
+            $dec = base_convert($number, $frombase, $tobase);
+            $pieces[] = $dec;
+        }
+        $str = implode(';&#', $pieces);
+        $string = "&#$str;";
+        return $string;
+    }
+
 }
