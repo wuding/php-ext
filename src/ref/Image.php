acb5e54fb8adab0ee3c2d9379139158cd658e450
wuding <netjoin@gmail.com>
2022-05-16 21:34:04 +0800

    新增 GD 和图像处理函数类，添加目录属性

---
+++ src/ref/Image.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * GD and Image Functions
+ *
+ * https://www.php.net/manual/en/ref.image.php
+ *
+ *
+ */
+
+namespace Ext\ref;
+
+class Image
+{
+    const VERSION = '22.5.16';
+
+    public $table_of_contents = array(
+        'gd_info' => array('gd-info', 'Retrieve information about the currently installed GD libary'),
+
+        'imagecolortransparent' => array('', 'Define a color as transparent'),
+
+        'png2wbmp' => array('', 'Convert PNG image file to WBMP image file'),
+    );
+}
