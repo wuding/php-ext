db5e8119fa2ac35fbac5d5a1ba7909f87bd0304c
wuding <netjoin@gmail.com>
2022-12-18 11:33:52 +0800

    增加 pow 和 PCRE 的 11 个函数

--- src/Appendices/Indexes/FunctionAndMethodListing.php
+++ src/Appendices/Indexes/FunctionAndMethodListing.php
@@ -5207,5 +5207,17 @@ class FunctionAndMethodListing
         'posix_times' => 'Get process times',
         'posix_ttyname' => 'Determine terminal device name',
         'posix_uname' => 'Get system name',
+        'pow' => 'Exponential expression',
+        'preg_filter' => 'Perform a regular expression search and replace',
+        'preg_grep' => 'Return array entries that match the pattern',
+        'preg_last_error' => 'Returns the error code of the last PCRE regex execution',
+        'preg_last_error_msg' => 'Returns the error message of the last PCRE regex execution',
+        'preg_match' => 'Perform a regular expression match',
+        'preg_match_all' => 'Perform a global regular expression match',
+        'preg_quote' => 'Quote regular expression characters',
+        'preg_replace' => 'Perform a regular expression search and replace',
+        'preg_replace_callback' => 'Perform a regular expression search and replace using a callback',
+        'preg_replace_callback_array' => 'Perform a regular expression search and replace using callbacks',
+        'preg_split' => 'Split string by a regular expression',
     );
 }
