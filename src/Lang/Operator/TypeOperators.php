6d576d14c6229fc68ac1ae13d640edbb2a4de459
Benny <netjoin@gmail.com>
2023-07-06 17:54:53 +0800

    Conduction \Lang\Operator\TypeOperators

---
+++ src/Lang/Operator/TypeOperators.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ext\Lang\Operator;
+
+class TypeOperators
+{
+    const VERSION = '23.7.6';
+    const REVISION = 1;
+
+    public static $para = array(
+        'instanceof is used to determine whether a PHP variable is an instantiated object of certain class',
+    );
+}
