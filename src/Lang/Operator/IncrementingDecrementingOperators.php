21ddf4f4e7a13b42784fa236bbd75da4009630e3
Benny <netjoin@gmail.com>
2023-07-06 18:24:30 +0800

    Conduction \Lang\Operator\IncrementingDecrementingOperators

---
+++ src/Lang/Operator/IncrementingDecrementingOperators.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ext\Lang\Operator;
+
+class IncrementingDecrementingOperators
+{
+    const VERSION = '23.7.6';
+    const REVISION = 1;
+
+    public static $para = array(
+        '++$a Pre-increment',
+        '$a++ Post-increment',
+        '--$a Pre-decrement',
+        '$a-- Post-decrement',
+    );
+}
