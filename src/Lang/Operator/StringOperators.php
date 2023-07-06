e16b9973c16dd1a628e2a08b6aba88728e3bb71a
Benny <netjoin@gmail.com>
2023-07-06 17:54:28 +0800

    Conduction \Lang\Operator\StringOperators

---
+++ src/Lang/Operator/StringOperators.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ext\Lang\Operator;
+
+class StringOperators
+{
+    const VERSION = '23.7.6';
+    const REVISION = 1;
+
+    public static $para = array(
+        "The first is the concatenation operator ('.')",
+        "The second is the concatenating assignment operator('.=')",
+    );
+}
