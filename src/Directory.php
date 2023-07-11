573256b4608d1b2122a0f1a1d6408a2d4b7f45f3
Benny <netjoin@gmail.com>
2023-07-11 12:44:36 +0800

    Conducts \Directory $predefined_contants Function scan() Parameters $options[array_filter_callback]

--- src/Directory.php
+++ src/Directory.php
@@ -4,10 +4,41 @@ namespace Ext;
 
 class Directory extends _Abstract
 {
-    const VERSION = '23.6.9';
+    const VERSION = '23.7.11';
+    const REVISION = 2;
 
-    public static function scan($directory, $sorting_order = SCANDIR_SORT_ASCENDING, $context = null)
+    public static $predefined_constants = array(
+        /* string */
+        'DIRECTORY_SEPARATOR',
+        'PATH_SEPARATOR',
+
+        /* scan() int */
+        'SCANDIR_SORT_ASCENDING',
+        'SCANDIR_SORT_DESCENDING',
+        'SCANDIR_SORT_NONE',
+    );
+
+
+    /*
+    +---------------------------------------+
+    + list
+    +---------------------------------------+
+    */
+
+    public static function scan($directory, $sorting_order = SCANDIR_SORT_ASCENDING, $context = null, $options = array())
     {
-        return $directories = scandir($directory, $sorting_order);
+        $excepts = array('.', '..');
+        $array_filter_callback = null;
+        extract($options);
+
+        $result_values = $directories = scandir($directory, $sorting_order);
+
+        if ($array_filter_callback) {
+            $result_values = array_filter($directories, $array_filter_callback);
+        }
+
+        return $result_values;
     }
+    //: array or false
+
 }
