c108f16d26ca09ff4cbb1a01ccd5d4e3f6bab1ea
Benny <netjoin@gmail.com>
2023-07-16 12:26:25 +0800

    Conducting Props $runtime_configuration Function __construct() code

--- src/Sess.php
+++ src/Sess.php
@@ -5,7 +5,7 @@ namespace Ext;
 class Sess
 {
     const VERSION = '23.7.16';
-    const REVISION = 2;
+    const REVISION = 3;
 
     public static $predefined_constants = array(
         /* session_id() string */
@@ -16,6 +16,54 @@ class Sess
         'PHP_SESSION_NONE',
         'PHP_SESSION_ACTIVE',
     );
+
+    public static $runtime_configuration = array(
+        'PHP_INI_ALL' => array(
+            'session.save_path' => "",
+            'session.name' => 'PHPSESSID',
+            'session.save_handler' => 'files',
+            'session.auto_start' => 0,
+            'session.gc_probability' => 1,
+            'session.gc_divisor' => 100,
+            'session.gc_maxlifetime' => 1440,
+            'session.serialize_handler' => 'php',
+            'session.cookie_lifetime' => 0,
+            'session.cookie_path' => '/',
+            'session.cookie_domain' => '',
+            'session.cookie_secure' => '',
+            'session.cookie_httponly' => '',
+            'session.cookie_samesite' => '',
+            'session.use_strict_mode' => 0,
+            'session.use_cookies' => 1,
+            'session.use_only_cookies' => 1,
+            'session.referer_check' => '',
+            'session.cache_limiter' => 'nocache',
+            'session.cache_expire' => 180,
+            'session.use_trans_sid' => 0,
+            'session.trans_sid_tags' => 'a=href,area=href,frame=src,form=',
+            'session.trans_sid_hosts' => null,
+            'session.sid_length' => 32,
+            'session.sid_bits_per_character' => 4,
+            'session.lazy_write' => 1,
+            'url_rewirter.tags' => 'a=href,area=href,frame=src,form=',
+            'session.hash_function' => 0,
+            'session.hash_birts_per_character' => 4,
+            'session.entropy_file' => '',
+            'session.entropy_length' => 0,
+            'session.bug_compat_42' => 1,
+            'session.bug_compat_warn' => 1,
+        ),
+        'PHP_INI_PREDIR' => array(
+            'session.upload_progress.enabled' => 1,
+            'session.upload_progress.cleanup' => 1,
+            'session.upload_progress.prefix' => 'upload_progress_',
+            'session.upload_progress.name' => 'PHP_SESSION_UPLOAD_PROGRESS',
+            'session.upload_progress.freq' => '1%',
+            'session.upload_progress.min_freq' => 1,
+
+        ),
+    );
+
 
 
 
@@ -28,6 +76,12 @@ class Sess
 
     public function __construct()
     {
+        static::$runtime_configuration['PHP_INI_ALL']['session.trans_sid_hosts'] = $_server_http_host = $_SERVER['HTTP_HOST'];
+
+        var_dump($expression = [__FILE__, __LINE__,
+            'runtime_configuration' => static::$runtime_configuration,
+            'vars' => get_defined_vars(),
+        ]);
 
     }
 
