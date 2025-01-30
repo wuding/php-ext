f0e2b6a6ac232d397993d06c4800a25cde57ec14
wuding <netjoin@gmail.com>
2025-01-31 04:11:04 +0800

    Conduction example/url/index

---
+++ example/url/index.php
@@ -0,0 +1,72 @@
+<?php
+
+// version 250112.1
+
+define('ROOT', dirname(__DIR__, 5));
+$autoload = require ROOT .'/vendor/autoload.php';
+
+use function php\func\get;
+use Ext\URL;
+
+class Example
+{
+    public function __construct()
+    {
+        $param_arr = get();
+        $method =  ltrim($_SERVER['PATH_INFO'] ?? '', '/');
+        $variable = get_defined_vars();
+        foreach ($variable as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    public function __destruct()
+    {
+        $this->form();
+        if ($this->param_arr) {
+            $this->run();
+        }
+    }
+
+    function form()
+    {
+        $variable = URL::$args_type[$this->method];
+        $args = URL::$args[$this->method];
+        $html = '';
+        $i = 0;
+        foreach ($variable as $key => $value) {
+            $val = $this->param_arr[$key] ?: $args[$i];
+            $html .= <<<HEREDOC
+<li>
+<label>
+$value \${$key}
+<input name="$key" value="$val" style="width:100%" onfocus="select()">
+</label>
+</li>
+HEREDOC;
+            $i++;
+        }
+
+        $form = <<<HEREDOC
+<form>
+$html
+<button type="submit">submit</button>
+</form>
+HEREDOC;
+
+        echo $form;
+    }
+
+    function run()
+    {
+        $function = array('\\Ext\\URL', $this->method ?: 'exists');
+
+        $expression = call_user_func_array($function, $this->param_arr);
+
+        echo '<pre>';
+        print_r($expression);
+        echo '</pre>';
+    }
+}
+
+new Example();
