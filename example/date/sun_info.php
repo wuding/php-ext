b493ffa10842c0d18f2b72cb59a2865ce2164635
wuding <netjoin@gmail.com>
2025-01-31 04:01:20 +0800

    Conducting example/date/sun_info Function compare()

--- example/date/sun_info.php
+++ example/date/sun_info.php
@@ -11,8 +11,8 @@ use function php\func\get;
 
 class SunInfo
 {
-    const VERSION = '24.6.6';
-    const REVISION = 3;
+    const VERSION = 25.0130;
+    const REVISION = 4;
 
     public static function thisYear($variable, $latitude, $longitude)
     {
@@ -42,58 +42,91 @@ class SunInfo
 
         return $array;
     }
-}
 
-$year = 2024;
-$latitude = 41.8619;
-$longitude = 123.9017;
+    static function run($latitude, $longitude)
+    {
+        $pathinfo_filename = pathinfo(__FILE__, PATHINFO_FILENAME);
 
-/*
-31.6699
-118.4645
-*/
+        $method = \Ext\Date::funcToMethodName($pathinfo_filename);
+        $function = array('\\Ext\\Date', $method);
+        $param_arr = get(array(
+            'timestamp' => time(),
+            'latitude' => $latitude,
+            'longitude' => $longitude,
+            'options' => array('return_values' => 1, 'timezone' => 'PRC'),
+        ));
 
-$method = \Ext\Date::funcToMethodName(pathinfo(__FILE__, PATHINFO_FILENAME));
-$function = array('\\Ext\\Date', $method);
-$param_arr = get(array(
-    'timestamp' => time(),
-    'latitude' => $latitude,
-    'longitude' => $longitude,
-    'options' => array('return_values' => 1, 'timezone' => 'PRC'),
-));
+        $param_arr['options']['return_values'] = (int) $param_arr['options']['return_values'];
 
-$param_arr['options']['return_values'] = (int) $param_arr['options']['return_values'];
-
-$expression = call_user_func_array($function, $param_arr);
-
-$arr = array();
-foreach ($expression['date_sun_info'] as $key => $value) {
-    $arr[$key] = date('Y-m-d H:i:s', $value);
-}
-$expression['date_sun_info_time'] = $arr;
-$expression['include'] = $include;
-$expression['DaysInMonth::thisYear'] = DaysInMonth::thisYear($year);
-
-$months = array();
-
-foreach ($expression['DaysInMonth::thisYear']['arr'] as $k => $val) {
-    $month = $val[0]['month'];
-    $days = $val[1] + 1;
-
-    $d = array();
-    for ($i = 1; $i < $days; $i++) {
-        $date_time = "$year-$k-$i";
-        $timestamp = strtotime($date_time);
-        $d[] = $timestamp;
+        return $expression = call_user_func_array($function, $param_arr);
     }
-    $sun = SunInfo::thisYear($d, $latitude, $longitude);
 
-    $s = array();
-    foreach ($sun as $key => $value) {
-        $s[] = $value['date_sun_info_time']['sunrise'];
+    static function month($year, $expression, $latitude, $longitude)
+    {
+        $arr = array();
+        foreach ($expression['date_sun_info'] as $key => $value) {
+            $arr[$key] = date('Y-m-d H:i:s', $value);
+        }
+        $expression['date_sun_info_time'] = $arr;
+        // $expression['include'] = $include;
+        $expression['DaysInMonth::thisYear'] = DaysInMonth::thisYear($year);
+
+        $months = array();
+
+        foreach ($expression['DaysInMonth::thisYear']['arr'] as $k => $val) {
+            $month = $val[0]['month'];
+            $days = $val[1] + 1;
+
+            $d = array();
+            for ($i = 1; $i < $days; $i++) {
+                $date_time = "$year-$k-$i";
+                $timestamp = strtotime($date_time);
+                $d[] = $timestamp;
+            }
+            $sun = SunInfo::thisYear($d, $latitude, $longitude);
+
+            $s = array();
+            foreach ($sun as $key => $value) {
+                $s[] = $value['date_sun_info_time']['sunrise'];
+            }
+            $months[] = $s;
+        }
+        return $months;
     }
-    $months[] = $s;
-}
-print_r($months);
 
-// print_r($expression);
+    static function runMonth($year, $latitude, $longitude)
+    {
+        $expression = SunInfo::run($latitude, $longitude);
+        return $a = SunInfo::month($year, $expression, $latitude, $longitude);
+    }
+
+    static function compare($year)
+    {
+        $latitude = 41.8619;
+        $longitude = 123.9017;
+
+        $a = SunInfo::runMonth($year, $latitude, $longitude);
+
+        $latitude = 31.6699;
+        $longitude = 118.4645;
+
+        $b = SunInfo::runMonth($year, $latitude, $longitude);
+
+        $arr = [];
+        foreach ($b as $key => $value) {
+            $val = $a[$key];
+            foreach ($val as $k => $v) {
+                $vb = $value[$k];
+                $sa = substr($v, 0, 16);
+                $sb = substr($vb, 0, 16);
+                if ($sa === $sb) {
+                    $arr[] = [$v, $vb];
+                }
+            }
+        }
+        return $arr;
+    }
+}
+
+$compare = SunInfo::compare(get('year', 2025));
+print_r($compare);
