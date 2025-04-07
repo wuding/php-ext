f32a5f705b154633bfbe94c8bbc89b502695ccd8
wuding <netjoin@gmail.com>
2025-04-07 15:40:31 +0800

    \stdClass

---
+++ src/stdClass.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Ext;
+
+class stdClass
+{
+    const VERSION = 25.0206;
+    const REVISION = 1;
+}
+
+/*
+If I Never Loved You (Disco Music)
+6867
+Bao Binh
+7409
+All between love and what it has cost
+一切尽在爱与代价之间
+03:19
+*/
