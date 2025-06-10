9a8977003b40b7274f26d0c64fed2fd9adce632a
wuding <netjoin@gmail.com>
2025-06-10 12:43:13 +0800

    Commutation example/mb/index.php print_r() Function var_dump()

--- example/mb/index.php
+++ example/mb/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// version 20240811
+// version 2.250610123749
 
 define('ROOT', dirname(__DIR__, 5));
 $autoload = require ROOT .'/vendor/autoload.php';
@@ -13,6 +13,6 @@ $method =  ltrim($_SERVER['PATH_INFO'] ?? '', '/');
 $function = array('\\Ext\\MB', $method ?: 'strCount');
 
 $expression = call_user_func_array($function, $param_arr);
-print_r($expression);
+var_dump($expression);
 
 // /example/mb/index.php/strCount?string=抓娃娃
