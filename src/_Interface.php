24519d50ac90b95e45f32abac27aa24be45e0de6
wuding <netjoin@gmail.com>
2025-07-14 13:25:07 +0800

    Commutation \_Interface Static Funciton _call()

--- src/_Interface.php
+++ src/_Interface.php
@@ -6,10 +6,10 @@ const CON = 1;
 
 interface _Interface
 {
-    const VER = 25.0206;
-    const REV = 2;
+    const VER = 25.0714;
+    const REV = 3;
 
     public function __construct();
 
-    public function _call($func, $args);
+    static function _call($func, $args);
 }
