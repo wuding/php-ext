9d5d75b6a82fde286cb3179a033f26fd36c88f59
wuding <netjoin@gmail.com>
2025-11-12 16:11:14 +0800

    $3 Conducts \URL Function rawurldecode()

--- src/URL.php
+++ src/URL.php
@@ -4,8 +4,8 @@ namespace Ext;
 
 class URL extends _Abstract
 {
-    const VERSION = 25.0725;
-    const REVISION = 2;
+    const VERSION = 25.1108;
+    const REVISION = 3;
 
     static $func = [
         '__construct' => [
@@ -25,14 +25,36 @@ class URL extends _Abstract
         'base64_decode' => [
             'string' => 'string',
         ],
+        'base64_encode' => [
+            'string' => 'string',
+        ],
     ];
 
-    function parse_url()
+    // 1. 编解码
+    static function base64_decode()
     {
-        return $this->_call(__FUNCTION__, func_get_args());
+        return self::_call(__FUNCTION__, func_get_args());
+    }
+    static function base64_encode()
+    {
+        return self::_call(__FUNCTION__, func_get_args());
     }
 
-    static function base64_decode()
+    static function rawurldecode(){}
+    static function rawurlencode(){}
+
+    static function urldecode(){}
+    static function urlencode(){}
+
+    // 2. 获取 HTTP 头和元标签
+    static function get_headers(){}
+    static function get_meta_tags(){}
+
+    // 3. 拼接成查询字符串
+    static function http_build_query(){}
+
+    // 4. 解析定位地址
+    static function parse_url()
     {
         return self::_call(__FUNCTION__, func_get_args());
     }
