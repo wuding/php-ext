cfbebae8b48fbc40eee3e40a4d96dd8a89bfa778
wuding <netjoin@gmail.com>
2026-05-10 20:39:17 +0800

    635=$7 Conducts \PDObj Function pre()

--- src/PDObj.php
+++ src/PDObj.php
@@ -4,7 +4,8 @@ namespace Ext;
 
 class PDObj
 {
-    const VERSION = '23.6.16';
+    const REVISION = 7;
+    const VERSION = 26.0510;
 
     // 运行时
     public static $connects = array();
@@ -76,5 +77,46 @@ class PDObj
             return $sth;
         }
         return $all = $sth->fetchAll();
+    }
+
+    function pre($sql = null, $var_array = [], $params = null, $func = 'fetchAll')
+    {
+        $fetchMode = \PDO::FETCH_OBJ;
+        // FETCH_OBJ FETCH_NAMED FETCH_ASSOC FETCH_NUM FETCH_BOTH [\PDO::FETCH_COLUMN, 1] FETCH_DEFAULT
+        extract($var_array);
+        if (is_array($params)) {
+            foreach ($params as $key => $value) {
+                $replacement = $this->quote($value);
+                // $sql = preg_replace("#$key#", $replacement, $sql);
+            }
+        }
+
+        // $sth = $this->query($sql);
+        $sth = $this->prepare($sql);
+        if (false === $sth) {
+            return $sth;
+        }
+
+        // $sth->execute(['"家"']);
+        $execute = $sth->execute($params);
+        // $colcount = $sth->columnCount();
+
+        if (is_array($fetchMode)) {
+            list($mode, $arg) = $fetchMode;
+            $sth->setFetchMode($mode, $arg);
+        } else {
+            $sth->setFetchMode($fetchMode);
+        }
+
+        $all = $sth->$func();
+/*
+        $sth->bindColumn('name', $name);
+        while ($sth->fetch(\PDO::FETCH_BOUND)) {
+            print join("\t", [$name]) . "\n";
+        }
+*/
+        // $queryString = $sth->queryString;
+        // var_dump(get_defined_vars());die;
+        return $all;
     }
 }
